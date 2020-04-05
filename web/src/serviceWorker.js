6f3e4cb0b940c4224f706f938651d0c85d742c5d
Nicholas Romano <nicholasromano@Nicholass-MBP.T-mobile.com>
2020-04-05 17:20:58 -0400

    ui fixes

--- web/src/serviceWorker.js
+++ web/src/serviceWorker.js
@@ -32,7 +32,8 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const noCache = new Date() * 1;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js?nocache=${noCache}`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
